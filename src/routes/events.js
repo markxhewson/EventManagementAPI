b02df77e6c40812b503eba91c019995c174faff1
thurasw <thurasw@icloud.com>
2024-03-21 10:25:38 +0000

    DB changes + accept webp uploads

--- src/routes/events.js
+++ src/routes/events.js
@@ -26,7 +26,7 @@ const imageUpload = multer({
     fileSize: 1024 * 1024 * 5 // 5MB,
   },
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png' || file.mimetype === 'image/webp') {
       cb(null, true)
     } else {
       cb(new Error("Invalid file type. Only images are allowed."))
